d05be9b5ac242c72ca7276aaeba0352d4ce941bb
Chris Lucas <chris.lucas@egplusww.com.au>
2016-03-11 17:31:33 +1100

    chore(environment): Removing env
    Removing the environment files and functionality due to it being irrelevant

--- .env.sample.js
+++
@@ -1,6 +0,0 @@
-// Copy this file to .env.js.
-module.exports = {
-  // 'chrome', 'firefox', 'safari', 'ie'
-  // IE, Windows and Linux have not been tested, fully working on OSX.
-  DEFAULT_BROWSER: "chrome",
-};
